1459492dd644fe52242fd7a91e9f66e36784c75f
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-10-20 18:39:47 -0300

    Criacao das classes principais

---
+++ clinica.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Paciente {
+    
+};
+
+class Data {
+    
+};
+
+class Medico {
+    
+};
+
+int main(){
+
+    cout << "Hello World";
+    return 0;
+}
