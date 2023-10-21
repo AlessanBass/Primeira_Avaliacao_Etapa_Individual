69b30c211f1b75e57a9307f1e9cde6fc87b4e58c
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-10-21 01:19:21 -0300

    Criação dos menus e implementação de algumas classes

--- clinica.cpp
+++ clinica.cpp
@@ -11,6 +11,9 @@ class Data {
     int ano;
 
     public:
+    //Construtor
+     Data(int _dia, int _mes, int _ano) 
+        : dia(_dia), mes(_mes), ano(_ano) {}
 
     //Setters
     void setDia(int _dia){
@@ -49,6 +52,9 @@ class Paciente {
     Data dtNascimento;
 
     public:
+    //Construtor
+     Paciente(string _cpf, string _nomePaciente, Data _dtNascimento)
+        : cpf(_cpf), nomePaciente(_nomePaciente), dtNascimento(_dtNascimento) {}
 
     //Setters
     void setCpf(string _cpf){
@@ -68,9 +74,13 @@ class Paciente {
         return this->nomePaciente;
     }
 
-    //Vou passar um vector como refencia para que seja adicionado nele
-    void incluir(vector<Paciente> &pacientes){
+    Data getDtNascimento(){
+        return this->dtNascimento;
+    }
 
+    //Vou passar um vector como refencia para que seja adicionado nele
+    void incluir(vector<Paciente> &pacientes, Paciente p){
+        pacientes.push_back(p);
     }
 
     void excluir(){
@@ -81,13 +91,22 @@ class Paciente {
 
     }
 
-    void listar(){
+    void listar(vector<Paciente> pacientes){
+        for(auto paciente : pacientes){
+           /*  cout << "-----------------------" << endl;
+            cout << "Nome: " << paciente.getNomePaciente() << endl;
+            cout << "CPF: " << paciente.getCpf() << endl;
+            cout << "Data de Nascimento: " << paciente.getDtNascimento().getDia();
+            cout <<" / " paciente.getDtNascimento().getMes();
+            cout << " / " paciente.getDtNascimento().getAno() << endl;; */
 
+        }
     }
 
     void localizarPorCpf(){
 
     }
+
 
 };
 
@@ -129,7 +148,128 @@ class Medico {
 };
 
 int main(){
+    
+    int opcao;
+    int opcaoSubMenu;
+    vector <Paciente> pacientes;
+    vector <Medico> medicos;
+    
 
-    cout << "Hello World";
+    //cout << "Tamanaho de pacientes: " << pacientes.size() << endl;
+
+    do{
+        cout << ">>> BEM-VINDO(A) A CLINICA SEM DODOI <<<" << endl;
+        cout << "[1] PACIENTES " << endl;
+        cout << "[2] MEDICOS " << endl;
+        cout << "[0] SAIR " << endl;
+        cin >> opcao;
+
+        switch(opcao){
+            case 0:
+            break;
+            //REFATORAR ESSE MENU 
+            case 1:
+            do{
+                cout << ">>> PACIENTES <<<" << endl;
+                cout << "[1] INCLUIR" << endl;
+                cout << "[2] EXCLUIR" << endl;
+                cout << "[3] ALTERAR" << endl;
+                cout << "[4] LISTAR" << endl;
+                cout << "[5] LOCALIZAR" << endl;
+                cout << "[0] SAIR" << endl;
+                cin >> opcaoSubMenu;
+
+                switch(opcaoSubMenu){
+                    case 0:
+                    break;
+
+                    case 1:
+                        {   system("cls");
+                            Data d(10,02,1999);
+                            Paciente paciente("0000", "0000", d);
+                            paciente.incluir(pacientes, paciente);
+                            cout << "Tamanaho de pacientes: " << pacientes.size() << endl;
+                        }
+                    break;
+
+                    case 2:
+                    break;
+
+                    case 3:
+                    break;
+
+                    case 4:
+                        system("cls");
+                        cout << "------- LISTANDO PACIENTES -------" << endl;
+                        for(auto paciente : pacientes){
+                            cout << "----------------------------------" << endl;
+                            cout << "Nome: " << paciente.getNomePaciente() << endl;
+                            cout << "CPF: " << paciente.getCpf() << endl;
+                            cout << "Data de Nascimento: " << paciente.getDtNascimento().getDia();
+                            cout << " / " << paciente.getDtNascimento().getMes();
+                            cout << " / " << paciente.getDtNascimento().getAno() << endl;
+                        }
+                        cout << "----------------------------------" << endl;
+                    break;
+
+                    case 5:
+                    break;
+
+                    default:
+                        system("cls");
+                        cout << "Opcao invalida! " << endl;
+                    break;
+                }
+
+
+            }while(opcaoSubMenu != 0);
+            break;
+
+            case 2:
+            do{
+                cout << ">>> MEDICOS <<<" << endl;
+                cout << "[1] INCLUIR" << endl;
+                cout << "[2] EXCLUIR" << endl;
+                cout << "[3] ALTERAR" << endl;
+                cout << "[4] LISTAR" << endl;
+                cout << "[5] LOCALIZAR" << endl;
+                cout << "[0] SAIR" << endl;
+                cin >> opcaoSubMenu;
+
+                switch(opcaoSubMenu){
+                    case 0:
+                    break;
+
+                    case 1:
+                    break;
+
+                    case 2:
+                    break;
+
+                    case 3:
+                    break;
+
+                    case 4:
+                    break;
+
+                    case 5:
+                    break;
+
+                    default:
+                        cout << "Opcao invalida! " << endl;
+                    break;
+                }
+
+
+            }while(opcaoSubMenu != 0);
+            break;
+
+            default:
+                cout << "Opcao invalida! " << endl;
+            break;
+        }
+
+    }while(opcao !=  0);
+
     return 0;
 }
